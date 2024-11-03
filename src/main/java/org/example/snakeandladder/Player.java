c64cb224dcb7c3e4fb68553d46bb9c0bfd712d24
Sanskar Agrawal <sanskaragrawal@Mac.lan>
2024-11-03 23:01:53 +0530

    [snl][Added base classes and methods in them][1/n]

--- src/main/java/org/example/snakeandladder/Player.java
+++ src/main/java/org/example/snakeandladder/Player.java
@@ -1,13 +1,12 @@
 package org.example.snakeandladder;
 
-import lombok.Data;
 
 public class Player {
     private int id;
     private String colour;
-    private Cell position;
+    private int position;
 
-    public Player(int id, String colour, Cell position){
+    public Player(int id, String colour, int position){
         this.colour = colour;
         this.id = id;
         this.position = position;
